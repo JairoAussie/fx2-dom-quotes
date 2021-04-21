3993b2a0f7606a83cc2386e13c26f776846e4213
jairoAussie <jairoaussie@gmail.com>
2021-04-21 10:06:57 +1000

    index.html and arrays ready for the lesson

---
+++ script.js
@@ -0,0 +1,62 @@
+let quotes = [
+    {
+      quote:"Hard work beats talent when talent doesn’t work hard.",
+      source:"Tim Notke",
+    },
+    {
+      quote:"It’s hard to beat a person who never gives up.",
+      source:"Babe Ruth",
+      year:1930,
+      sport:"Baseball player"
+    },
+    {
+      quote:"The harder the battle, the sweeter the victory.",
+      source:"Les Brown",
+      sport:"Boxer"
+  
+    },
+    {
+      quote:"Never say never because limits, like fears, are often just an illusion.",
+      source:"Michael Jordan",
+      citation:"Chicago Tribune",
+      sport:"Basketball player"
+    },
+    {
+      quote:"You miss 100% of the shots you don’t take",
+      source:"Wayne Gretzky"
+    },
+    {
+      quote:"Today, you have 100% of your life left.",
+      source:"Tom Landry",
+    },
+    {
+      quote:"You have to expect things of yourself before you can do them.",
+      source:"Michael Jordan",
+      citation:"Chicago",
+      sport:"Basketball player"
+    },
+    {
+      quote:"Winning isn’t everything, but wanting to win is.",
+      source:"Vince Lombardi"
+    },
+    {
+      quote:"There may be people that have more talent than you, but there’s no excuse for anyone to work harder than you do.",
+      source:"Derek Jeter"
+    },
+    {
+      quote:"I became a good pitcher when I stopped trying to make them miss the ball and started trying to make them hit it.",
+      source:"Sandy Koufax",
+      year: 1950
+    }];
+
+//data from https://gist.github.com/mucar/3898821
+var colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
